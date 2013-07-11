3766b997c5d20c4c93cb2ee2fc28225aa9b3dc0a
kazuya katsuhara <katsuhara@key-p.co.jp>
2013-07-11 09:34:08 +0900

    db.phpを修正

--- fuelphp/sql/create_table.sql
+++ fuelphp/sql/create_table.sql
@@ -1,10 +1,10 @@
 set names utf8;
 
-drop table if exists list; create table list (
+drop table if exists information; create table information (
 id int(8) auto_increment, primary key (id)
 ,subject varchar(250) NULL default NULL
 ,detail varchar(250) NULL default NULL
-,del_flg TINYINT(1) default '0'
+,del_at timestamp NULL default NULL
 ,in_dt datetime NULL default NULL
 ,up_dt datetime NULL default NULL
 )
