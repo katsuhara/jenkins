cc64072812a9c5092e43fb900d57ea2916b95ab0
tomoko UCHIDA <uchida@key-p.co.jp>
2013-07-18 10:31:43 +0900

    フィールド旧名称「del_at」を新名称「del_flg」に変更

--- fuelphp/fuel/app/classes/model/information.php
+++ fuelphp/fuel/app/classes/model/information.php
@@ -2,33 +2,33 @@
 
 class Model_Information extends \Orm\Model_Soft
 {
-	protected static $_properties = array(
-		'id',
-		'subject',
-		'detail',
-        'del_at',
-		'in_dt',
-		'up_dt',
-	);
+    protected static $_properties = array(
+        'id',
+        'subject',
+        'detail',
+        'del_flg',
+        'in_dt',
+        'up_dt',
+    );
 
-	protected static $_observers = array(
-		'Orm\Observer_CreatedAt' => array(
-			'events' => array('before_insert'),
-			'mysql_timestamp' => true,
-			'property' => 'in_dt',
-		),
-		'Orm\Observer_UpdatedAt' => array(
-			'events' => array('before_update'),
-			'mysql_timestamp' => true,
-			'property' => 'up_dt',
-		),
-	);
+    protected static $_observers = array(
+        'Orm\Observer_CreatedAt' => array(
+            'events' => array('before_insert'),
+            'mysql_timestamp' => true,
+            'property' => 'in_dt',
+        ),
+        'Orm\Observer_UpdatedAt' => array(
+            'events' => array('before_update'),
+            'mysql_timestamp' => true,
+            'property' => 'up_dt',
+        ),
+    );
 
-	protected static $_soft_delete = array(
-		'mysql_timestamp' => true,
-		'deleted_field' => 'del_at',
-	);
-	protected static $_table_name = 'information';
+    protected static $_soft_delete = array(
+        'mysql_timestamp' => true,
+        'deleted_field' => 'del_flg',
+    );
+    protected static $_table_name = 'information';
 
     public static function validate($factory)
     {
