0f643f9c29f51e85620b8aa00cd08a1b67c2998f
kpyamasaki <yamasaki@key-p.co.jp>
2013-08-30 09:54:23 +0900

    コミット漏れ追加

--- fuelphp/fuel/app/tests/model/information.php
+++ fuelphp/fuel/app/tests/model/information.php
@@ -7,12 +7,40 @@
  */
 class Test_Model_Information extends TestCase
 {
-    public function test_foo()
+    /**
+     * @dataProvider invalid_data_provider
+     */
+    public function test_不正なデータはバリデーションが通らない(array $src)
     {
-        $info = Model_Information::forge();
+        $val = Model_Information::forge()->validate('test_invalid');
 
-        $expected = '';
+        $this->assertFalse($val->run($src));
+    }
+    public function invalid_data_provider()
+    {
+        return array(
+            array(array('subject' => '', 'detail' => '')),
+            array(array('subject' => 'dummy', 'detail' => '')),
+            array(array('subject' => str_repeat('abc', 100), 'detail' => 'dummy')),
+            array(array('subject' => 'dummy', 'detail' => str_repeat('abc', 100))),
+            array(array('subject' => str_repeat('abc', 100), 'detail' => str_repeat('abc', 100))),
+        );
+    }
 
-        $this->assertEquals($expected, $info->id);
+    /**
+     * @dataProvider valid_data_provider
+     */
+    public function test_正しいデータはバリデーションが通る(array $src)
+    {
+        $val = Model_Information::forge()->validate('test_valid');
+
+        $this->assertTrue($val->run($src));
+    }
+    public function valid_data_provider()
+    {
+        return array(
+            array(array('subject' => 'dummy', 'detail' => 'dummy')),
+            array(array('subject' => str_repeat('a', 250), 'detail' => str_repeat('b', 250))),
+        );
     }
 }
